f9b3e92e87925a20f9b2ba1f4e3cac3b70810692
mounika <mounikamusham12@gmail.com>
2021-12-15 18:23:44 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,11 @@
+function myCar(name) {
+    console.log("my car name is" + " "+ name);
+
+}
+myCar("cat");
+
+//
+myCar("dog");
+function myPet(name) {
+    console.log("my car name is" + " "+ name);
+}
